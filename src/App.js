024a7b6127ca458671a1791e8727cd967a9e810b
yagmurD <https://github.com/yagmur9234>
2022-01-20 18:16:42 +0300

    n

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import TopBar from "./components/common/TopBar";
 import Footer from "./components/common/Footer";
 import CustomRoutes from "./router/CustomRoutes";
 import { BrowserRouter, Routes } from "react-router-dom";
-import PageHeader from "./components/common/PageHeader";
+
 import Menu from "./components/common/Menu";
 
 function App() {
